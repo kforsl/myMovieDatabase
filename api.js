2ca47a2a1465bcaf990f70689f19389045044240
kforsl <forslund95@live.com>
2024-02-19 16:06:56 +0100

    fetchMore function

--- api.js
+++ api.js
@@ -1,3 +1,9 @@
+const apiKey = `810608b8`;
+
+async function fetchMore(id) {
+    return await fetchApi(`http://www.omdbapi.com/?apikey=${apiKey}&plot=full&i=${id}`)
+}
+
 async function fetchApi(apiUrl) {
     const results = await fetch(apiUrl);
     return await results.json()
